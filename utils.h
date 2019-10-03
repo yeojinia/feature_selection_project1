b345e8ca379ad668b5662084b2948709edd69a39
Yeajin Kim <kimyea@flip2.engr.oregonstate.edu>
2019-10-02 20:46:57 -0700

    utils.h

---
+++ utils.h
@@ -0,0 +1,18 @@
+#pragma once
+#pragma warning(disable : 4996)
+
+#include <cstdio>
+#include <ctime> // to generate random number
+#include <sstream> // to use getline()
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <malloc.h>
+#include <Windows.h>
+
+#include "Datasets.h"
+#include "LinearRegression.h"
+#include "ReadData.h"
+
+using namespace std;
